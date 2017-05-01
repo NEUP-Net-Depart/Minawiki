3ed3e856ab7d8c8c0885da6971433a5577b04329
WMJ <w.mingjian@yahoo.com>
2017-05-01 13:20:23 +0800

    用户中心的面包屑导航

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -19,14 +19,21 @@ class UserController extends Controller
             return (new AuthController()) -> showLoginView($request);
         }
         // 面包屑导航
-        $page = Page::all();
-        $currentPage = $page -> where('id', 3) -> first();
         $path = collect([]);
+        $page = Page::all();
+//        $currentPage = $page -> where('id', 3) -> first();
+        // 为了使用面包屑导航, 但是不能把用户中心加入Page表
+        $currentPage = new Page();
+        $currentPage -> title = "用户中心";
+        $currentPage -> father_id = '1';
+        $currentPage -> id = '9999';
+
         while ($currentPage['id'] != 1) {
             $path -> prepend($currentPage);
             $currentPage = $currentPage -> where('id', $currentPage['father_id']) -> first();
         }
         $path -> prepend($currentPage);
+
 
         // 获得登录的用户并跳转
         $userid = $request -> session() -> get('user.id');
