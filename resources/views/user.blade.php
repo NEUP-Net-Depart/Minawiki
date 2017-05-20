d80be78c88c1ca0ecec78d5f4bd3ec2f250df9d8
Master_2572 <cj846798404@gmail.com>
2017-05-20 14:20:13 +0000

    user.blade.php is for test

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,40 +1,4 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Laravel</title>
 
-        <style>
-            html, body {
-                height: 100%;
-            }
-
-            body {
-                margin: 0;
-                padding: 0;
-                width: 100%;
-                display: table;
-                font-weight: 100;
-                font-family: 'Lato';
-            }
-
-            .container {
-                text-align: center;
-                display: table-cell;
-                vertical-align: middle;
-            }
-
-            .content {
-                text-align: center;
-                display: inline-block;
-            }
-
-            .title {
-                font-size: 20px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="container">
                 @foreach($message['commentId'] as $value)
 
                        content:{{$message['content'][$value]}}<br/>
@@ -46,9 +10,3 @@
                             starUser{{$message['starUser'][$value][$i]}}
                         @endfor
                <br><br> @endforeach
-        </div>
-                <p></p>
-            </div>
-        </div>
-    </body>
-</html>
